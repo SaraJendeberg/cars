b6c077753858e6b7030c30086d89d31dfda97cb1
Sara Jendeberg <sarajendeberg@Schnuttbook-Pro.local>
2018-11-18 23:31:42 +0100

    Add error handling on POST

--- home/views.py
+++ home/views.py
@@ -32,6 +32,12 @@ def carmodels(request):
         return JsonResponse(data, safe=False, json_dumps_params={'ensure_ascii':False})
     if request.method == "POST":
         received_data = json.loads(request.body) #turn body into dictionary
+        if (int(received_data["price"])<=0):
+            return JsonResponse("Don't enter a negative price or a price of 0. Try again!", safe=False,
+                                json_dumps_params={'ensure_ascii': False})
+        elif (len(received_data["brand"]) == 0 or len(received_data["model"]) == 0):
+            return JsonResponse("Don't enter an empty string. Try again!", safe=False,
+                                json_dumps_params={'ensure_ascii': False})
         data = dbfunctions.addNewCarModel(received_data["brand"], received_data["model"], int(received_data["price"]))
         print(data)
         return JsonResponse(data, safe=False, json_dumps_params={'ensure_ascii' : False})
